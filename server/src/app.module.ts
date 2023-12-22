1f1af3eb9d1bf24ff0ec909280113ec756437dd0
khevin1 <khevin@delman.io>
2023-12-22 18:53:44 +0700

    set docker compose and try to connect prisma service between providers

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 import { BookmarksModule } from './bookmarks/bookmarks.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, BookmarksModule],
+  imports: [AuthModule, UsersModule, BookmarksModule, PrismaModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
