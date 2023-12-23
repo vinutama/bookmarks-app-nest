0aece26502f3e2cee58544835756fe07ceb02f45
khevin1 <khevin@delman.io>
2023-12-23 17:05:33 +0700

    validate email exists users

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import * as argon from 'argon2';
 
 @Injectable()
@@ -11,26 +12,36 @@ export class AuthService {
     async register(dto: AuthDto) {
         // generate hash password using argon2
         const password = await argon.hash(dto.password);
+        
         // save the user in the DB
-        const user = await this.prisma.users.create({
-            data: {
-                email: dto.email,
-                password,
-            },
-            // select cols to returning
-            // select: {
-            //     id: true,
-            //     email: true,
-            //     firstName: true,
-            //     lastName: true,
-            // }
-        });
-
-        // or just exclude the fields
-        delete user.password;
-
-        // return the user info details
-        return user;
+        try {
+            const user = await this.prisma.users.create({
+                data: {
+                    email: dto.email,
+                    password,
+                },
+                // select cols to returning
+                // select: {
+                //     id: true,
+                //     email: true,
+                //     firstName: true,
+                //     lastName: true,
+                // }
+            });
+    
+            // or just exclude the fields
+            delete user.password;
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError){
+                // check error message code here
+                // https://www.prisma.io/docs/orm/reference/error-reference#error-codes
+                if (error.code == 'P2002'){
+                    throw new ForbiddenException('Email already taken');
+                }
+            }
+            throw error;
+        }
     }
 
     login() {
